9ed6ad65738e43a0e0c20db30b20e1b1edc5f89e
victor880415 <victoryan24512184>
2018-05-28 23:20:51 +0800

    更動
    我不知道如何用drawString
    https://yunlinsong.blogspot.tw/2016/10/java.html
    這是我摻考得網址

--- a0526/Shapes.java
+++ a0526/Shapes.java
@@ -8,14 +8,15 @@ public class Shapes
    public static void main( String[] args )
    {
       
-      JFrame frame = new JFrame( "Drawing 2D shapes" );
+      JFrame frame = new JFrame( "¼²²y" );
       frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 
       
       ShapesJPanel shapesJPanel = new ShapesJPanel();
       frame.add( shapesJPanel ); 
-      frame.setSize( 350, 700 ); 
+      frame.setSize( 350, 725 ); 
       frame.setVisible( true ); 
+      
    } 
 } 
 
